1d992d927b4f1cb02d6d95e15f702afce82883db
Isaias Pomales <impomales@gmail.com>
2018-03-30 13:00:09 -0400

    can enter a guess using either the enter key or submit button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-	
+	const game = newGame();
+
+	$('#submit').on('click', game, handleSubmit);
+	$('#player-input').keyup(game, handleKeyUp);
 });
+
+function handleSubmit(e) {
+	const game = e.data;
+	const input = $('#player-input');
+	const guess = Number(input.val());
+	input.val('');
+
+	console.log(game.playersGuessSubmission(guess));
+}
+
+function handleKeyUp(e) {
+	const code = e.keyCode || e.which;
+	if (code === 13) handleSubmit({data: e.data});
+}
